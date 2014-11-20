c8be8bd914538ef6a7c45f230ddea94f69345098
Madison Koenig <pedlar88@gmail.com>
2014-11-19 20:11:01 -0600

    Initial gh-pahes

--- library/src/main/java/org/notlocalhost/sqliteadapter/models/WhereStatement.java
+++
@@ -1,9 +0,0 @@
-package org.notlocalhost.sqliteadapter.models;
-
-/**
- * Created by pedlar on 9/7/14.
- */
-public class WhereStatement {
-    public String whereClause;
-    public String[] whereArgs;
-}
